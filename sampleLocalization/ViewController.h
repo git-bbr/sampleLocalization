31ac579f556c3125cd2c5b59207c4c18fb8b6538
bharath <bharath@bharaths-mac.local>
2016-03-22 09:31:51 -0700

    Initial Commit

---
+++ sampleLocalization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleLocalization
+//
+//  Created by bharath on 22/03/16.
+//  Copyright © 2016 bharath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sampleLocalization/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sampleLocalization
+//
+//  Created by bharath on 22/03/16.
+//  Copyright © 2016 bharath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
